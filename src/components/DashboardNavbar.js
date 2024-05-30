14c471ba7f3f3b452bf76b91a00be6a7e973af47
ainipei0112 <ainipei0112@gmail.com>
2024-05-30 14:50:10 +0800

    05/30 優化DashboardNavbar

--- src/components/DashboardNavbar.js
+++ src/components/DashboardNavbar.js
@@ -1,30 +1,26 @@
-import { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import {
   AppBar,
   Badge,
-  Box,
-  Hidden,
   IconButton,
   Toolbar,
+  useMediaQuery,
+  useTheme,
   Tooltip,
   Fade
 } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
+import MenuIcon from '@material-ui/icons/Menu';
 import Logo from './Logo';
 
-const DashboardNavbar = ({ onMobileNavOpen, ...rest }) => {
-  const [notifications] = useState([]);
+const DashboardNavbar = ({ onMobileNavOpen }) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('lg'));
+  const notifications = []; // 假設沒有通知
 
   return (
-    <AppBar
-      elevation={0}
-      color="primary"
-      {...rest}
-    >
+    <AppBar elevation={0} color="primary">
       <Toolbar>
         <Tooltip
           TransitionComponent={Fade}
@@ -36,38 +32,33 @@ const DashboardNavbar = ({ onMobileNavOpen, ...rest }) => {
             <Logo />
           </RouterLink>
         </Tooltip>
-        <Box sx={{ flexGrow: 1 }} />
-        <Hidden lgDown>
-          <IconButton color="inherit">
-            <Badge
-              badgeContent={notifications.length}
-              color="primary"
-              variant="dot"
-            >
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <IconButton color="inherit">
-            <RouterLink to="/login">
-              <InputIcon />
-            </RouterLink>
-          </IconButton>
-        </Hidden>
-        <Hidden lgUp>
-          <IconButton
-            color="inherit"
-            onClick={onMobileNavOpen}
-          >
+        <div style={{ flexGrow: 1 }} />
+        {!isSmallScreen && (
+          <>
+            <IconButton color="inherit">
+              <Badge
+                badgeContent={notifications.length}
+                color="primary"
+                variant="dot"
+              >
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton color="inherit">
+              <RouterLink to="/login">
+                <InputIcon />
+              </RouterLink>
+            </IconButton>
+          </>
+        )}
+        {isSmallScreen && (
+          <IconButton color="inherit" onClick={onMobileNavOpen}>
             <MenuIcon />
           </IconButton>
-        </Hidden>
+        )}
       </Toolbar>
     </AppBar>
   );
-};
-
-DashboardNavbar.propTypes = {
-  onMobileNavOpen: PropTypes.func
 };
 
 export default DashboardNavbar;

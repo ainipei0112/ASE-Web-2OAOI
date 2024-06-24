5a44b3d5f6ff3ae6d58d6cb093256c54b37dc903
ainipei0112 <ainipei0112@gmail.com>
2024-06-24 11:17:11 +0800

    06/24 更新表格首頁尾頁按鈕寫法

--- src/components/product/ProductListResults.js
+++ src/components/product/ProductListResults.js
@@ -1,14 +1,8 @@
 import { Autocomplete, Box, Card, Checkbox, TextField } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { makeStyles, useTheme } from "@mui/styles";
+import { makeStyles } from "@mui/styles";
 import { useContext, useEffect, useReducer, useMemo, useState } from "react";
 import { AppContext } from "src/Context";
-
-import IconButton from "@mui/material/IconButton";
-import FirstPageIcon from "@mui/icons-material/FirstPage";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import LastPageIcon from "@mui/icons-material/LastPage";
 
 // 調整下拉選單和表格間距
 const useStyles = makeStyles((theme) => ({
@@ -70,68 +64,6 @@ const calculateGroupedProducts = (filteredProducts) => {
     return acc;
   }, {});
 };
-
-function TablePaginationActions(props) {
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </Box>
-  );
-}
 
 const ProductListResults = () => {
   const classes = useStyles();
@@ -290,7 +222,8 @@ const ProductListResults = () => {
                 quickFilterProps: { debounceMs: 500 },
               },
               pagination: {
-                ActionsComponent: TablePaginationActions,
+                showFirstButton: true,
+                showLastButton: true,
               },
             }}
             sx={{
